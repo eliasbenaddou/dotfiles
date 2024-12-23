61caf7a028a9fcc6968d4c7f460a2096564ff91a
Elias Benaddou Idrissi <e.benaddou@hey.com>
2024-12-23 19:58:12 +0000

    Adding .zshrc file and removing .zhenv file

--- dot_config/zsh/custom.zsh
+++ dot_config/zsh/custom.zsh
@@ -8,8 +8,42 @@ eval "$(starship init zsh)"
 # uv shell completion
 eval "$(uv generate-shell-completion zsh)"
 
-# fzf
+# Themes (onedark or nord)
+export STARSHIP_THEME="onedark"
+export WEZTERM_THEME="onedark"
+
+# Locale settings
+export LANG="en_GB.UTF-8" # Sets default locale for all categories
+export LC_ALL="en_GB.UTF-8" # Overrides all other locale settings
+export LC_CTYPE="en_GB.UTF-8" # Controls character classification and case conversion
+
+# Add /usr/local/bin to the beginning of the PATH environment variable.
+# This ensures that executables in /usr/local/bin are found before other directories in the PATH.
+export PATH="/usr/local/bin:$PATH"
+
+# Hide computer name in terminal
+export DEFAULT_USER="$(whoami)"
+
+# Setup fzf
 eval "$(fzf --zsh)"
+
+# -- Use fd instead of fzf --
+export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git"
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
+
+# Use fd (https://github.com/sharkdp/fd) for listing path candidates.
+# - The first argument to the function ($1) is the base path to start traversal
+# - See the source code (completion.{bash,zsh}) for the details.
+_fzf_compgen_path() {
+  fd --hidden --exclude .git . "$1"
+}
+
+# Use fd to generate the list for directory completion
+_fzf_compgen_dir() {
+  fd --type=d --hidden --exclude .git . "$1"
+}
+
 
 # Case insensitive autosuggestions
 zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
@@ -30,4 +64,3 @@ source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 HISTFILE=$HOME/.zhistory
 SAVEHIST=1000
 HISTSIZE=999
-
---
+++ dot_zshrc
@@ -0,0 +1,5 @@
+# Custom zsh
+[ -f "$HOME/.config/zsh/custom.zsh" ] && source "$HOME/.config/zsh/custom.zsh"
+
+# Aliases
+[ -f "$HOME/.config/zsh/aliases.zsh" ] && source "$HOME/.config/zsh/aliases.zsh"

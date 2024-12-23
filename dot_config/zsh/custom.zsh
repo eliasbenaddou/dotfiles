67502be4f1ee4ef2b77a7f0d522992ccf975de8c
Elias Benaddou Idrissi <e.benaddou@hey.com>
2024-12-23 20:19:26 +0000

    Refactored back to last changes from Nov 2024 due to issues

--- dot_config/zsh/custom.zsh
+++ dot_config/zsh/custom.zsh
@@ -5,46 +5,6 @@ export EDITOR="hx"
 export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
 eval "$(starship init zsh)"
 
-# uv shell completion
-eval "$(uv generate-shell-completion zsh)"
-
-# Themes (onedark or nord)
-export STARSHIP_THEME="onedark"
-export WEZTERM_THEME="onedark"
-
-# Locale settings
-export LANG="en_GB.UTF-8" # Sets default locale for all categories
-export LC_ALL="en_GB.UTF-8" # Overrides all other locale settings
-export LC_CTYPE="en_GB.UTF-8" # Controls character classification and case conversion
-
-# Add /usr/local/bin to the beginning of the PATH environment variable.
-# This ensures that executables in /usr/local/bin are found before other directories in the PATH.
-export PATH="/usr/local/bin:$PATH"
-
-# Hide computer name in terminal
-export DEFAULT_USER="$(whoami)"
-
-# Setup fzf
-eval "$(fzf --zsh)"
-
-# -- Use fd instead of fzf --
-export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git"
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
-
-# Use fd (https://github.com/sharkdp/fd) for listing path candidates.
-# - The first argument to the function ($1) is the base path to start traversal
-# - See the source code (completion.{bash,zsh}) for the details.
-_fzf_compgen_path() {
-  fd --hidden --exclude .git . "$1"
-}
-
-# Use fd to generate the list for directory completion
-_fzf_compgen_dir() {
-  fd --type=d --hidden --exclude .git . "$1"
-}
-
-
 # Case insensitive autosuggestions
 zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
 autoload -Uz compinit && compinit

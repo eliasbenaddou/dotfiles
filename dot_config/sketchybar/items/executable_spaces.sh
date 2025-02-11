c3bd6e40b3ed34e23a587f993a6eee58e77ce80f
Elias Benaddou Idrissi <e.benaddou@hey.com>
2025-02-11 19:22:13 +0000

    Adding reader workspace to aerospace and mapping to sketchybar

--- dot_config/sketchybar/items/executable_spaces.sh
+++ dot_config/sketchybar/items/executable_spaces.sh
@@ -4,7 +4,7 @@
 sketchybar --add event aerospace_workspace_change
 
 # Define your spaces with names and corresponding Nerd Font icons
-SPACES=("home:" "web:󰖟" "email:󰇮" "code:" "work:" "music:󰎆" "chat:󰍩")
+SPACES=("home:" "web:󰖟" "reader:" "email:󰇮" "code:" "work:" "music:󰎆" "chat:󰍩")
 
 # Add and configure spaces
 for SPACE in "${SPACES[@]}"; do

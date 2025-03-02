dad0147ab18ead38fa7288d837a13a66949639fd
Elias Benaddou Idrissi <e.benaddou@hey.com>
2025-03-02 16:10:30 +0000

    Adding functionality to switch between effort workspaces

--- dot_config/wezterm/config.lua
+++ dot_config/wezterm/config.lua
@@ -1,9 +1,8 @@
 local wezterm = require("wezterm")
 local config = {}
-
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
+local lwl = wezterm.plugin.require("https://github.com/estin/lazy-workspace-layout?rev=master")
+local home = wezterm.home_dir
+local projects_dir = home .. "/efforts_on"
 
 local function resize_pane(key, direction)
   return {
@@ -20,6 +19,32 @@ local function move_pane(key, direction)
   }
 end
 
+local function get_project_workspaces()
+  local workspaces = {}
+  local p = io.popen('find "' .. projects_dir .. '" -mindepth 1 -maxdepth 1 -type d 2>/dev/null')
+  if p then
+    for dir in p:lines() do
+      local project_name = dir:match(".*/(.*)")
+      table.insert(workspaces, { cwd = dir, label = project_name })
+    end
+    p:close()
+  end
+  return workspaces
+end
+
+local workspaces = get_project_workspaces()
+lwl.init(workspaces)
+
+wezterm.on('refresh-workspaces', function(window, pane)
+  local new_workspaces = get_project_workspaces()
+  lwl.update_workspaces(new_workspaces)
+  window:toast_notification('WezTerm', 'Workspaces refreshed', nil, 4000)
+end)
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
 config.default_cursor_style = "SteadyBar"
 config.automatically_reload_config = true
 config.window_close_confirmation = "NeverPrompt"
@@ -28,9 +53,9 @@ config.window_decorations = "RESIZE"
 config.check_for_updates = false
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = false
+config.enable_tab_bar = false
 config.font_size = 15
 config.font = wezterm.font("JetBrains Mono", { weight = "Bold" })
-config.enable_tab_bar = false
 config.window_padding = { left = 3, right = 3, top = 0, bottom = 0 }
 
 config.hyperlink_rules = {
@@ -47,6 +72,8 @@ config.keys = {
   { key = "RightArrow", mods = "OPT", action = wezterm.action.SendString "\x1bf" },
   { key = "v", mods = "LEADER", action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" } },
   { key = "s", mods = "LEADER", action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" } },
+  { key = "w", mods = "LEADER", action = lwl.bind() },
+  { key = "r", mods = "LEADER", action = wezterm.action.EmitEvent("refresh-workspaces") },
   move_pane("j", "Down"),
   move_pane("k", "Up"),
   move_pane("h", "Left"),
@@ -71,6 +98,6 @@ config.key_tables = {
   }
 }
 
-config.leader = { key = "w", mods = "CTRL", timeout_milliseconds = 1000 }
+config.leader = { key = "w", mods = "OPT", timeout_milliseconds = 1000 }
 
 return config

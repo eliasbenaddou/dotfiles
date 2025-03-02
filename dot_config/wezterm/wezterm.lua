4b0f05ea62bc1d46e811867e722586dc59bc5959
Elias Benaddou Idrissi <e.benaddou@hey.com>
2025-03-02 13:41:51 +0000

    Updating config file

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -2,17 +2,14 @@ local wezterm = require("wezterm")
 local config = require("config")
 require("events")
 
+-- Apply color scheme based on the WEZTERM_THEME environment variable
 local themes = {
-  nord = "Nord (Gogh)",
-  onedark = "One Dark (Gogh)",
-  github_dark = "Github Dark (Gogh)"
+	nord = "Nord (Gogh)",
+	onedark = "One Dark (Gogh)",
+	github_dark = "Github Dark (Gogh)"
 }
-
--- Retrieve the WEZTERM_THEME environment variable
 local success, stdout, stderr = wezterm.run_child_process({ os.getenv("SHELL"), "-c", "printenv WEZTERM_THEME" })
-
--- Remove all whitespace characters (including newline) and apply the theme
-local selected_theme = stdout:gsub("%s+", "")
+local selected_theme = stdout:gsub("%s+", "") -- Remove all whitespace characters including newline
 config.color_scheme = themes[selected_theme]
 
 return config

3f09d4e4613e46de1c80f648ae41ee964b2c8f48
Elias Benaddou Idrissi <e.benaddou@hey.com>
2025-03-10 10:39:45 +0000

    Changing theme to tokyo night storm and removing gpt lsp from helix

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -2,12 +2,10 @@ local wezterm = require("wezterm")
 local config = require("config")
 require("events")
 
--- Apply color scheme based on the WEZTERM_THEME environment variable
 local themes = {
-	nord = "Nord (Gogh)",
-	onedark = "One Dark (Gogh)",
-	github_dark = "Github Dark (Gogh)"
+	tokyo_night_storm = "Tokyo Night Storm",
 }
+
 local success, stdout, stderr = wezterm.run_child_process({ os.getenv("SHELL"), "-c", "printenv WEZTERM_THEME" })
 local selected_theme = stdout:gsub("%s+", "") -- Remove all whitespace characters including newline
 config.color_scheme = themes[selected_theme]
--- dot_config/zsh/completions.zsh
+++ dot_config/zsh/completions.zsh
@@ -6,6 +6,7 @@ alias ls='ls --color'
 
 # Case-insensitive autosuggestions
 zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
+
 # Colour in completions
 zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
 autoload -Uz compinit && compinit
